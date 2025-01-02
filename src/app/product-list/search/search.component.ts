d7067499075c5589747d938bee34c3215c283e3f
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2025-01-02 09:12:21 +0100

    Two ways data binding

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  searchText: string="Mens wears";
 
+  updateText(event: any) {
+    this.searchText=event.target.value
+
+  }
 }
