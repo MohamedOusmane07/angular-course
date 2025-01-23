b929f20d4c15d8eabd88952f2fdf7f4e652a22b2
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2025-01-23 11:14:45 +0100

    Unrelated Component communication

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -7,47 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ContainerComponent {
 
-  addToCart:number = 0;
-  displayValue:string="";
-  product ={
-    name : "Iphone 13",
-    price: 867 ,
-    color : "white",
-    discount : 6.8,
-    inStock: 5,
-    pImage : "/assets/images/iphone13.jpeg"
+  searchTextFromAppSearch:string='';
 
+  setSearchText(value:string){
+    this.searchTextFromAppSearch=value;
   }
-
-  getDiscounted(){
-    return this.product.price - (this.product.price * this.product.discount / 100)
-  }
-
-  decrementCartValue(){
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-
-  }
-
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-
-  }
-
-  addT() {
-    console.log("dfslmfj")
-  }
-
-  //protected readonly event = event;
-
-  displayText(event:any) {
-    console.log(event);
-    this.displayValue=event.target.value;
-
-  }
-
-//  protected readonly event = event;
 }
--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,21 +1,83 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
+
 export class SearchComponent {
   searchText: string="";
 
-  updateText(event: any) {
-    this.searchText=event.target.value
+  // create Event
+  @Output()
+  searchTextChanged:EventEmitter<string> = new EventEmitter<string>();
 
+  onSearchTextChanged(){
+    this.searchTextChanged.emit(this.searchText);
   }
-  displaySearchText(){
-    if (this.searchText!=''){
-      return true;
-    }
-    else return false;
-  }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+updateText(event: any) {
+  this.searchText=event.target.value
+
+}
+
+displaySearchText(){
+  if (this.searchText!=''){
+    return true;
+  }
+  else return false;
+}*/
