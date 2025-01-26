33d1b98405ed66a67f8dc1f00a7e031159ddfb97
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2025-01-26 14:30:37 +0100

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,4 @@ import {Component, ElementRef, QueryList, viewChild, ViewChildren,} from '@angul
 })
 export class AppComponent {
   title = 'angular-ekart';
-  fullName:string='';
-  @ViewChildren('inputEl')
-  inputElList:QueryList<ElementRef>;
-
-  show(){
-    let name = ''
-    this.inputElList.forEach((el)=>{
-      console.log(el.nativeElement.value)
-      name +=el.nativeElement.value + ' '
-    })
-    this.fullName=name.trim();
-  }
 }
