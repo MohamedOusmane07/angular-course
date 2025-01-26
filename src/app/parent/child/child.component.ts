3320de162f10df370fce24a95b56589d49841a3a
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2025-01-26 15:39:06 +0100

    ContentChildren

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -1,4 +1,4 @@
-import {Component, ContentChild, ElementRef} from '@angular/core';
+import {Component, ContentChild, ContentChildren, ElementRef, QueryList} from '@angular/core';
 import {TestComponent} from "../../test/test.component";
 
 @Component({
@@ -8,12 +8,23 @@ import {TestComponent} from "../../test/test.component";
 })
 export class ChildComponent {
 
-  @ContentChild('para') paragaphEl : ElementRef;
+  @ContentChild('para' ) paragaphEl : ElementRef;
   @ContentChild(TestComponent) testEl:TestComponent;
 
+  @ContentChildren('para') paraEls:QueryList<ElementRef>;
+
+  @ContentChildren(TestComponent) testEls : QueryList<TestComponent>;
+
   StyleParagraph() {
-    console.log(this.paragaphEl.nativeElement);
-    console.log(this.testEl.name)
+    //console.log(this.paragaphEl.nativeElement);
+    //console.log(this.testEl.name)
+
+    this.paraEls.forEach((el:ElementRef)=>{
+      console.log(el.nativeElement);
+    })
+    this.testEls.forEach((test:TestComponent)=>{
+      console.log(test);
+    })
 
   }
 }
